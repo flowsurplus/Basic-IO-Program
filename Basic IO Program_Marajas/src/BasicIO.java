ee6766ebd8a617b5e9b5ddf619d1e680e992be7a
flowsurplus <51942477+flowsurplus@users.noreply.github.com>
2020-01-28 17:47:03 -0500

    Add files via upload

---
+++ Basic IO Program_Marajas/src/BasicIO.java
@@ -0,0 +1,29 @@
+// Michael Jay Marajas Basic IO Program
+// COP 2800C 
+// 1/22/2020
+// Purpose: To showcase user input and ability to perform mathematical functions using the input
+import java.util.Scanner;
+
+class BasicIO {
+  public static void main(String[] args) {
+	// Utilizing the Scanner object to get user input
+    Scanner myObj = new Scanner(System.in);
+
+    System.out.println("Enter in 3 numbers followed by an Enter:");
+
+    // User input for 3 Numerical Values
+    
+    int firstNumber = myObj.nextInt();
+    int secondNumber = myObj.nextInt();
+    int thirdNumber = myObj.nextInt();
+
+    // Requested Function from the assignment
+    int sum = firstNumber + secondNumber + thirdNumber;
+    double avg = (firstNumber + secondNumber + thirdNumber) / 3;
+    
+    // Results from User Input
+    
+    System.out.println("Sum: " + sum);
+    System.out.println("Average: " + avg);
+  }
+}
